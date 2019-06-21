46b9f4cd4cbab02dba21faa3e1715d682825d1e0
fdy696 <3456283371@qq.com>
2019-06-21 17:36:30 +0800

    新增权限校验函数式组件

---
+++ src/components/Permission.vue
@@ -0,0 +1,17 @@
+<script>
+import { checkAccess } from "../utils/permission";
+
+export default {
+  functional: true,
+  props: {
+    authority: {
+      type: Array,
+      required: true
+    }
+  },
+  render(h, context) {
+    const { props, scopedSlots } = context;
+    return checkAccess(props.authority) ? scopedSlots.default() : null;
+  }
+};
+</script>
