9b560a52e51aa302c38c0caad505987eab5d8228
fdy696 <a1111@bogon.www.tendawifi.com>
2019-06-21 17:15:39 +0800

    路由鉴权

--- src/main.js
+++ src/main.js
@@ -1,11 +1,35 @@
 import Vue from "vue";
-import { Button } from "ant-design-vue";
+import {
+  Button,
+  Icon,
+  Layout,
+  Breadcrumb,
+  Drawer,
+  Radio,
+  Menu,
+  Avatar,
+  Badge,
+  Dropdown
+} from "ant-design-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "ant-design-vue/dist/antd.css";
+import Permission from "./components/Permission";
 Vue.config.productionTip = false;
 
 Vue.use(Button);
+Vue.use(Layout);
+Vue.use(Icon);
+Vue.use(Breadcrumb);
+Vue.use(Drawer);
+Vue.use(Radio);
+Vue.use(Menu);
+Vue.use(Avatar);
+Vue.use(Badge);
+Vue.use(Dropdown);
+
+Vue.component("Permission", Permission);
 
 new Vue({
   router,
--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import Router from "vue-router";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
+import findLast from "lodash/findLast";
+import { checkAccess, isLogin } from "./utils/permission";
 Vue.use(Router);
 
 const router = new Router({
@@ -11,18 +13,20 @@ const router = new Router({
     // 用户相关路由
     {
       path: "/user",
+      name: "user",
+      hideInMenu: true,
       redirect: "/user/login",
       component: () => import("./layouts/UserLayout"),
       children: [
         {
           path: "login",
           name: "login",
-          component: () => import("./views/User/Login")
+          component: () => import("./views/user/Login")
         },
         {
           path: "register",
           name: "register",
-          component: () => import("./views/User/Register")
+          component: () => import("./views/user/Register")
         }
       ]
     },
@@ -30,6 +34,9 @@ const router = new Router({
     {
       path: "/",
       redirect: "/dashboard/analysis",
+      meta: {
+        authority: ["admin", "user"]
+      },
       component: () => import("./layouts/BasicLayout"),
       children: [
         {
@@ -39,28 +46,61 @@ const router = new Router({
           redirect: "/dashboard/workplace",
           meta: {
             title: "仪表盘",
-            icon: "dashboard",
-            permission: ["dashboard"]
+            icon: "dashboard"
           },
           children: [
             {
               path: "/dashboard/analysis",
               name: "Analysis",
               component: () => import("@/views/dashboard/Analysis"),
-              meta: { title: "分析页", permission: ["dashboard"] }
+              meta: { title: "分析页" }
             },
             {
               path: "/dashboard/monitor",
               name: "Monitor",
               hidden: true,
               component: () => import("@/views/dashboard/Monitor"),
-              meta: { title: "监控页", permission: ["dashboard"] }
+              meta: { title: "监控页" }
             },
             {
               path: "/dashboard/workplace",
               name: "Workplace",
               component: () => import("@/views/dashboard/Workplace"),
-              meta: { title: "工作台", permission: ["dashboard"] }
+              meta: { title: "工作台" }
+            }
+          ]
+        },
+        {
+          path: "/test",
+          name: "test",
+          component: () => import("./views/form/Test"),
+          meta: {
+            title: "测试",
+            icon: "smile",
+            authority: ["admin"]
+          }
+        },
+        {
+          path: "/form",
+          name: "form",
+          component: { render: h => h("router-view") },
+          meta: { icon: "form", title: "表单", authority: ["admin", "user"] },
+          children: [
+            {
+              path: "/form/basic-form",
+              name: "basicform",
+              meta: {
+                title: "基础表单"
+              },
+              component: () => import("./views/form/BasicForm")
+            },
+            {
+              path: "/form/step-form",
+              name: "stepform",
+              meta: {
+                title: "分布表单"
+              },
+              component: () => import("./views/form/StepForm")
             }
           ]
         }
@@ -69,6 +109,7 @@ const router = new Router({
     {
       path: "*",
       name: "404",
+      hideInMenu: true,
       component: () => import("./views/Notfound")
     }
   ]
@@ -76,6 +117,19 @@ const router = new Router({
 
 router.beforeEach((to, from, next) => {
   NProgress.start();
+  const record = findLast(to.matched, record => record.meta.authority);
+  if (record && !checkAccess(record.meta.authority)) {
+    if (!isLogin() && to.path !== "/user/login") {
+      next({
+        path: "/user/login"
+      });
+    } else if (to.path !== "/403") {
+      next({
+        path: "/403"
+      });
+    }
+    NProgress.done();
+  }
   next();
 });
 
