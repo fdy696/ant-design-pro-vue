4623d6b6fec6c2e99d3723300bc501b5bf7fffc2
fdy696 <3456283371@qq.com>
2019-06-21 17:38:23 +0800

    新增lodash,nprogress

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,12 @@
     <router-view />
   </div>
 </template>
+
+<style>
+html,
+body {
+  height: 100%;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, PingFang SC,
+    Hiragino Sans GB, Microsoft YaHei, Helvetica Neue, Helvetica, Arial,
+    sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol;
+}
